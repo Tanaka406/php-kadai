ec85c2b4a54488476c8206b532e1861612f9fd12
Tanaka_Yusuke <yusuke.kun2400@gmail>
2020-02-24 14:29:24 +0000

    first commit

---
+++ practice.php
@@ -0,0 +1,45 @@
+<?php
+echo "\n";
+$a = 3;
+$b = 7;
+echo $a+$b;
+echo "\n";
+
+$array_month = ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
+echo $array_month[7];
+echo "\n";
+
+$hello = "Hello,";
+$name = "tanaka";
+$world = "s World!";
+echo $hello,$name,$world;
+echo "\n";
+
+$tech_boost = "tech";
+$tech_boost .= " boost";
+echo $tech_boost;
+echo "\n";
+ 
+ $calendar_2018 = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+ echo $calendar_2018[11];
+ echo "\n";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
